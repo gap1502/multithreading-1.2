10ebc5d2356f52375b947bc4ac79971634102948
unknown <admin@mbaryshnikn7p.inlg.ru>
2022-01-27 15:53:18 +0500

    multithreading2 v2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,37 +1,29 @@
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.*;
 
 public class Main {
     public static void main(String[] args) {
+
+        List<MyCallable> callableList = new ArrayList<>();
         System.out.println("Создаю потоки...");
-        Callable<Integer> myCallable1 = new MyCallable("поток 1");
-        Callable<Integer> myCallable2 = new MyCallable("поток 2");
-        Callable<Integer> myCallable3 = new MyCallable("поток 3");
-        Callable<Integer> myCallable4 = new MyCallable("поток 4");
+        for (int i = 1; i <= 4; i++) {
+            callableList.add(new MyCallable("поток " + i));
+        }
 
         final ExecutorService threadPool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
-        final Future<Integer> task1 = threadPool.submit(myCallable1);
-        final Future<Integer> task2 = threadPool.submit(myCallable2);
-        final Future<Integer> task3 = threadPool.submit(myCallable3);
-        final Future<Integer> task4 = threadPool.submit(myCallable4);
 
         try {
-            final Integer result1 = task1.get();
-            final Integer result2 = task2.get();
-            final Integer result3 = task3.get();
-            final Integer result4 = task4.get();
-            System.out.println(myCallable1 + " отправил сообщения: " + result1 + " раз");
-            System.out.println(myCallable2 + " отправил сообщения: " + result2 + " раз");
-            System.out.println(myCallable3 + " отправил сообщения: " + result3 + " раз");
-            System.out.println(myCallable4 + " отправил сообщения: " + result4 + " раз");
+            List<Future<Integer>> task = threadPool.invokeAll(callableList);
+            for (int i = 0; i < task.size(); i++) {
+                System.out.println(callableList.get(i) + " отправил сообщения: " + task.get(i).get() + " раз");
+            }
         } catch (InterruptedException | ExecutionException e) {
             e.printStackTrace();
         }
 
         try {
-            List<Callable<Integer>> list = Arrays.asList(myCallable1, myCallable2, myCallable3, myCallable4);
-            final Integer resultQuick = threadPool.invokeAny(list);
+            final Integer resultQuick = threadPool.invokeAny(callableList);
             System.out.println("Результат самой быстрой задачи: " + resultQuick);
         } catch (InterruptedException | ExecutionException e) {
             e.printStackTrace();
